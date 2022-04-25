a9507b18773b9f3028a69a6145c8f68698d6e3e3
connor Bain <connor.bain@northwestern.edu>
2022-04-24 20:43:19 -0500

    wk5.1

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ python3 course-files/build/zip_it.py course-files/lectures
 python3 course-files/build/zip_it.py course-files/tutorials
 
 # Build the Project 2 docs
-pdoc course-files/projects/project02/apis/yelp.py -o course-files/projects/project02/docs --html --force
-pdoc course-files/projects/project02/apis/spotify.py -o course-files/projects/project02/docs --html --force
-pdoc course-files/projects/project02/apis/twilio.py -o course-files/projects/project02/docs --html --force
-pdoc course-files/projects/project02/apis/authentication.py -o course-files/projects/project02/docs --html --force
+pdoc course-files/projects/project02/apis/yelp.py -o course-files/projects/project02/docs
+pdoc course-files/projects/project02/apis/spotify.py -o course-files/projects/project02/docs
+pdoc course-files/projects/project02/apis/twilio.py -o course-files/projects/project02/docs
+pdoc course-files/projects/project02/apis/authentication.py -o course-files/projects/project02/docs
 
 python3 course-files/build/zip_it.py course-files/projects
 
