47e2e1c15e57ba8aa1029fe6c4d7a2abb7389b8c
connor Bain <connor.bain@northwestern.edu>
2022-05-10 00:05:14 -0500

    lecture17 update

--- course-files/lectures/lecture17/advanced/animate_rotate.py
+++ course-files/lectures/lecture17/advanced/animate_rotate.py
@@ -20,6 +20,12 @@ def make_creature(canvas, center, size=100, my_tag='creature', my_fill='hotpink'
     left_eye_pos = (center[0] - size / 4, center[1] - size / 5)
     right_eye_pos = (center[0] + size / 4, center[1] - size / 5)
     eye_width = eye_height = size / 10
+
+
+    ## Special Note: this rotate function doesn't work on circles and ovals so instead use
+    ## the make_poly_circle and make_poly_oval functions to make your circles and ovals. these
+    ## approximate the shape of a circle and oval by using a many sided polygon.
+
     utilities.make_poly_circle(canvas, center, size, fill_color=my_fill, tag=my_tag)
     utilities.make_poly_oval(canvas, left_eye_pos, eye_width, eye_height, fill_color='black', tag=my_tag)
     utilities.make_poly_oval(canvas, right_eye_pos, eye_width, eye_height, fill_color='black', tag=my_tag)
