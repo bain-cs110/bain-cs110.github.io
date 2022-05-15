3bb187b473533cc806789dc11741d0a934236f1a
connor Bain <connor.bain@northwestern.edu>
2022-05-15 18:09:37 -0500

    wk8 prep

--- course-files/lectures/lecture19/answers/01_error_handling/02_try_except.py
+++ course-files/lectures/lecture19/answers/01_error_handling/02_try_except.py
@@ -3,14 +3,12 @@ while True:
     age = input('enter your age: ')
     year = input('enter a year in the future: ')
     try:
-        print('Your age in 2050:', int(age) + int(year) - 2019)
+        print('Your age in 2050:', int(age) + int(year) - 2022)
     except:
-        # Note: this code uses the string "format" method.
         print('Both your age and year should be an integer')
-    
-    what_now = input('Do you want to try again (Y/n)?' )
+
+    what_now = input('Do you want to try again (Y/n)?')
     if what_now.upper() == 'N':
         # Exit the loop...
         print('exiting...\n')
         break
-    
