0f2b3270cb241cd70c881b974fd49f9dea0caeb3
connor Bain <connor.bain@northwestern.edu>
2022-06-03 16:56:19 -0500

    p2 docs update

--- build.sh
+++ build.sh
@@ -4,10 +4,9 @@ python3 course-files/build/zip_it.py course-files/lectures
 python3 course-files/build/zip_it.py course-files/tutorials
 
 # Build the Project 2 docs
-pdoc course-files/projects/project02/apis/yelp.py -o course-files/projects/project02/docs
-pdoc course-files/projects/project02/apis/spotify.py -o course-files/projects/project02/docs
-pdoc course-files/projects/project02/apis/twilio.py -o course-files/projects/project02/docs
-pdoc course-files/projects/project02/apis/authentication.py -o course-files/projects/project02/docs
+pdoc --show-source --logo "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Yelp_Logo.svg/800px-Yelp_Logo.svg.png" course-files/projects/project02/apis/yelp -o course-files/projects/project02/docs
+pdoc --show-source --logo "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Twilio-logo-red.svg/2560px-Twilio-logo-red.svg.png" course-files/projects/project02/apis/twilio -o course-files/projects/project02/docs
+pdoc --show-source --logo "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1024px-Spotify_logo_with_text.svg.png" course-files/projects/project02/apis/spotify -o course-files/projects/project02/docs
 
 python3 course-files/build/zip_it.py course-files/projects
 
