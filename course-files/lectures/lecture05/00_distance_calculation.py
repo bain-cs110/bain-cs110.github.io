7c9e565c89d2fb640dc8699b178855b3218ba72b
connor Bain <connor.bain@northwestern.edu>
2023-01-12 17:30:57 -0600

    wk2.3

---
+++ course-files/lectures/lecture05/00_distance_calculation.py
@@ -0,0 +1,8 @@
+speed = 80   # miles per hour
+time = 4.0   # hours
+distance = speed * time
+speed = 40
+unit = "miles"
+print(distance + unit)
+
+# Visualize the execution here: https://tinyurl.com/3pxfzwj4 
