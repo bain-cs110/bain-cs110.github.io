24c5893c84f2a0b1b74dee48454dbf5ce81c3028
connor Bain <connor.bain@northwestern.edu>
2023-02-09 20:14:35 -0600

    wk6.3

--- _site/build.sh
+++ _site/build.sh
@@ -2,11 +2,11 @@
 python3 course-files/build/zip_it.py course-files/homeworks
 python3 course-files/build/zip_it.py course-files/lectures
 python3 course-files/build/zip_it.py course-files/tutorials
-python3 course-files/build/zip_it.py course-files/extra-practice
 
 python3 course-files/build/zip_it.py ../bain-cs110/tutorial_solutions
 python3 course-files/build/zip_it.py ../bain-cs110/exercise_solutions
 
+pdoc --show-source course-files/projects/project01/project_utilities -o course-files/projects/project01/docs
 
 
 
