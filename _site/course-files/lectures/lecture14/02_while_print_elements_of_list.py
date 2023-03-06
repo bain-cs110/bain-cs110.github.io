d61b6ddf17d9f82bddc6b3de60264bd1decfa8ba
connor Bain <connor.bain@northwestern.edu>
2023-03-06 01:40:26 -0600

    remove _site

--- _site/course-files/lectures/lecture14/02_while_print_elements_of_list.py
+++
@@ -1,3 +0,0 @@
-names = ['Connor Bain', 'Shameik Moore', 'Kimiko Glenn', 'Tom Holland', 'Andrew Garfield', 'Tobey Maguire', 'Hailee Steinfeld']
-
-# write a while loop that prints every item in the names list:
