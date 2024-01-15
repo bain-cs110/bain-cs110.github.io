15ea686a85397b7f3b986a784f8a8a12004f51ad
connor Bain <connor.bain@northwestern.edu>
2024-01-15 01:17:01 -0600

    wk3

--- course-files/homeworks/hw02_template.py
+++ course-files/homeworks/hw02_template.py
@@ -1,87 +1,75 @@
-'''
-Documentation: https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/canvas.html
-Color Picker: https://coolors.co/ # spring 2023
-'''
 from tkinter import Canvas, Tk
+
 gui = Tk()
-gui.title('Shapes')
-the_canvas = Canvas(gui, width=500, height=500, background='white',
-                scrollregion = "0 -500 500 0")
-
+gui.title("Homework 2")
+the_canvas = Canvas(gui, width=500, height=500, background="white")
 the_canvas.pack()
-
 ########################## YOUR CODE BELOW THIS LINE ##############################
+def oval(center_x, center_y, radius_x, radius_y, color="hotpink"):
+    x = center_x
+    y = center_y
+    x0, y0, x1, y1 = (x - radius_x, y - radius_y, x + radius_x, y + radius_y)
+    steps = 100
+    # major and minor axes
+    a = (x1 - x0) / 2.0
+    b = (y1 - y0) / 2.0
+    # center
+    xc = x0 + a
+    yc = y0 + b
+    point_list = []
+    # create the oval as a list of points
+    for i in range(steps):
+        # Calculate the angle for this step
+        theta = (math.pi * 2) * (float(i) / steps)
+        x = a * math.cos(theta)
+        y = b * math.sin(theta)
+        point_list.append(round(x + xc))
+        point_list.append(round(y + yc))
 
-## MAKE_OVAL SECTION #####!!!#####
+    return the_canvas.create_polygon(point_list, fill=color)
+######################### YOUR CODE BELOW THIS LINE ###############################
 
-def make_oval(a_canvas, center_x, center_y, radius_x, radius_y, color='hotpink'):
-    a_canvas.create_oval(
-        100, 100,
-        200, 150,
-        fill='hotpink')
-
-# note, delete the below line when you copy the other tests in
-make_oval(the_canvas, 100, 100, 25, 40) 
-
-## MAKE_CIRCLE SECTION #####!!!#####
-
-
-
-
-
-
-
-
-## MAKE_BULLSEYE SECTION #####!!!#####
-
-
-
-
-
-
-
-## MAKE_FACE SECTION #####!!!#####
+## TASK 1
 
 
 
+## TASK 2
 
-    
 
 
-########################## YOUR CODE ABOVE THIS LINE ############################## 
+## TASK 3
+
+
+
+## TASK 4
+
+
+########################## YOUR CODE ABOVE THIS LINE ##############################
 # helper function that draws a grid.
 def make_grid(c, w, h):
     interval = 100
 
-    # Creates all vertical lines at intevals of 100
+    # Creates all vertical lines at intervals of 100
     for i in range(0, w, interval):
-        c.create_line(i, 0, i, h, tag='grid_line')
+        c.create_line(i, 0, i, h, tag="grid_line")
 
-    # Creates all horizontal lines at intevals of 100
+    # Creates all horizontal lines at intervals of 100
     for i in range(0, h, interval):
-        c.create_line(0, i, w, i, tag='grid_line')
+        c.create_line(0, i, w, i, tag="grid_line")
 
-    # spring 2023 marker
     # Creates axis labels
     offset = 2
     for y in range(0, h, interval):
         for x in range(0, w, interval):
-            c.create_oval(
-                x - offset, 
-                y - offset, 
-                x + offset,  
-                y + offset, 
-                fill='black'
-            )
+            c.create_oval(x - offset, y - offset, x + offset, y + offset, fill="black")
             c.create_text(
-                x + offset, 
-                y + offset, 
+                x + offset,
+                y + offset,
                 text="({0}, {1})".format(x, y),
-                anchor="nw", 
-                font=("Purisa", 8)
+                anchor="nw",
+                font=("Purisa", 8),
             )
-            
-make_grid(the_canvas, 500, 500)
 
-the_canvas.scale("all", 0, 0, 1, -1)
+
+make_grid(the_canvas, 500, 500)
 the_canvas.mainloop()
---
+++ course-files/lectures/lecture05/01b_using_input.py
@@ -0,0 +1,6 @@
+# ----------------------------------------------------
+# 1. say hello using an input
+# ----------------------------------------------------
+print("Hello there, Obi-wan!")
+print("Your name is", len("Obi-wan"), "letters long")
+print("How are you this morning?")
