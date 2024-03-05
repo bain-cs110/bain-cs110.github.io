00f1ac2f19626f084640634fd27407562e46ecd9
connor Bain <connor.bain@northwestern.edu>
2024-03-05 16:14:31 -0600

    upgrade installer to fix SSL on mac

--- course-files/projects/project2/install_python_packages.py
+++ course-files/projects/project2/install_python_packages.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import os.path, ssl, stat
 
 
 def restart_program():
@@ -63,8 +64,38 @@ while True:
 
         print("Running...", [python_path, '-m', 'pip',
               'install', '--upgrade', user_input.strip()])
-        result = subprocess.run(
-            [python_path, '-m', 'pip', 'install', '--upgrade', user_input.strip()], stdout=subprocess.PIPE)
+        
+        if user_input != "certifi":
+            result = subprocess.run(
+                [python_path, '-m', 'pip', 'install', '--upgrade', user_input.strip()], stdout=subprocess.PIPE)
+        
+        else:
+
+            STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+                        | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+                        | stat.S_IROTH |                stat.S_IXOTH )
+
+            openssl_dir, openssl_cafile = os.path.split(
+                ssl.get_default_verify_paths().openssl_cafile)
+
+            result = subprocess.run([sys.executable,
+                "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"], stdout=subprocess.PIPE)
+
+            import certifi
+
+            # change working directory to the default SSL directory
+            os.chdir(openssl_dir)
+            relpath_to_certifi_cafile = os.path.relpath(certifi.where())
+            print(" -- removing any existing file or link")
+            try:
+                os.remove(openssl_cafile)
+            except FileNotFoundError:
+                pass
+            print(" -- creating symlink to certifi certificate bundle")
+            os.symlink(relpath_to_certifi_cafile, openssl_cafile)
+            print(" -- setting permissions")
+            os.chmod(openssl_cafile, STAT_0o775)
+            print(" -- update complete")
 
         print("***** SYSTEM OUT *****\n",
               result.stdout.decode(), "\n***** SYSTEM OUT *****")
